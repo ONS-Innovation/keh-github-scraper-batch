4ee93562efb5c013079290a82f798f4ea093755d
delterr <Hasan.T.Ahmad@ons.gov.uk>
2025-06-11 14:51:00 +0100

    pattern up variable names

--- concourse/scripts/terraform_infra.sh
+++ concourse/scripts/terraform_infra.sh
@@ -2,22 +2,22 @@ set -euo pipefail
 
 apk add --no-cache jq
 
-aws_account_id=$(echo "$tat_secrets_ui" | jq -r .aws_account_id)
-aws_access_key_id=$(echo "$tat_secrets_ui" | jq -r .aws_access_key_id)
+aws_account_id=$(echo "$github_scraper_secrets" | jq -r .aws_account_id)
+aws_access_key_id=$(echo "$github_scraper_secrets" | jq -r .aws_access_key_id)
 
-aws_secret_access_key=$(echo "$tat_secrets_ui" | jq -r .aws_secret_access_key)
-api_secret_name=$(echo "$tat_secrets_ui" | jq -r .api_secret_name)
+aws_secret_access_key=$(echo "$github_scraper_secrets" | jq -r .aws_secret_access_key)
+domain=$(echo "$github_scraper_secrets" | jq -r .domain)
 
-domain=$(echo "$tat_secrets_ui" | jq -r .domain)
+source_bucket=$(echo "$github_scraper_secrets" | jq -r .source_bucket)
+source_key=$(echo "$github_scraper_secrets" | jq -r .source_key)
 
-api_bucket_name=$(echo "$tat_secrets_ui" | jq -r .api_bucket_name)
-ui_secret_name=$(echo "$tat_secrets_ui" | jq -r .ui_secret_name)
+github_app_client_id=$(echo "$github_scraper_secrets" | jq -r .source_bucket)
+aws_secret_name=$(echo "$github_scraper_secrets" | jq -r .aws_secret_name)
 
-aws_account_name=$(echo "$tat_secrets_ui" | jq -r .aws_account_name)
-localhost=$(echo "$tat_secrets_ui" | jq -r .localhost)
+github_org=$(echo "$github_scraper_secrets" | jq -r .github_org)
+container_image=$(echo "$github_scraper_secrets" | jq -r .container_image)
 
-container_image=$(echo "$tat_secrets_ui" | jq -r .container_image)
-force_deployment=$(echo "$tat_secrets_ui" | jq -r .force_deployment)
+batch_size=$(echo "$github_scraper_secrets" | jq -r .batch_size)
 
 export AWS_ACCESS_KEY_ID=$aws_access_key_id
 export AWS_SECRET_ACCESS_KEY=$aws_secret_access_key
@@ -34,13 +34,13 @@ terraform apply \
 -var "aws_account_id=$aws_account_id" \
 -var "aws_access_key_id=$aws_access_key_id" \
 -var "aws_secret_access_key=$aws_secret_access_key" \
--var "api_secret_name=$api_secret_name" \
 -var "domain=$domain" \
 -var "container_ver=${tag}" \
--var "api_bucket_name=$api_bucket_name" \
--var "ui_secret_name=$ui_secret_name" \
--var "aws_account_name=$aws_account_name" \
--var "localhost=$localhost" \
 -var "container_image=$container_image" \
--var "force_deployment=$force_deployment" \
+-var "source_bucket=$source_bucket" \
+-var "source_key=$source_key" \
+-var "github_app_client_id=$github_app_client_id" \
+-var "aws_secret_name=$aws_secret_name" \
+-var "github_org=$github_org" \
+-var "batch_size=$batch_size" \
 -auto-approve
