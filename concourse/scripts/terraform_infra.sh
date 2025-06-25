cd2cda48f8a5bc72edfe57ec9857e4bae5f1d1d5
delterr <Hasan.T.Ahmad@ons.gov.uk>
2025-06-25 12:13:31 +0100

    update build_image and terraform_infra with correct secrets

--- concourse/scripts/terraform_infra.sh
+++ concourse/scripts/terraform_infra.sh
@@ -15,21 +15,9 @@ github_app_client_id=$(echo "$github_scraper_secrets" | jq -r .github_app_client
 aws_secret_name=$(echo "$github_scraper_secrets" | jq -r .aws_secret_name)
 
 github_org=$(echo "$github_scraper_secrets" | jq -r .github_org)
-container_image=$(echo "$github_scraper_secrets" | jq -r .container_image)
+ecr_repository_name=$(echo "$github_scraper_secrets" | jq -r .ecr_repository_name)
 
 batch_size=$(echo "$github_scraper_secrets" | jq -r .batch_size)
-
-echo "AWS Account ID: $aws_account_id"
-echo "AWS Access Key ID: $aws_access_key_id"
-echo "AWS Secret Access Key: $aws_secret_access_key"
-echo "Domain: $domain"
-echo "Source Bucket: $source_bucket"
-echo "Source Key: $source_key"
-echo "GitHub App Client ID: $github_app_client_id"
-echo "AWS Secret Name: $aws_secret_name"
-echo "GitHub Organization: $github_org"
-echo "Container Image: $container_image"
-echo "Batch Size: $batch_size"
 
 export AWS_ACCESS_KEY_ID=$aws_access_key_id
 export AWS_SECRET_ACCESS_KEY=$aws_secret_access_key
@@ -57,5 +45,7 @@ terraform apply \
 -var "github_app_client_id=$github_app_client_id" \
 -var "aws_secret_name=$aws_secret_name" \
 -var "github_org=$github_org" \
+-var "ecr_repository_name=$ecr_repository_name" \
+-var "schedule=$schedule" \
 -var "batch_size=$batch_size" \
 -auto-approve
